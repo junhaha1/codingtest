7936614d2f3d512a6c64ba0b7cd5544ad0bd4d32
junhaha1 <wnsgk919@naver.com>
2025-02-27 17:25:21 +0900

    2회 풀이

--- baekjoon/step1/1874stacksequence.py
+++ baekjoon/step1/1874stacksequence.py
@@ -1,32 +1,29 @@
 from sys import stdin
+
 input = stdin.readline
 
+n = int(input())
 count = 1
-temp = True
 stack = []
 op = []
+fail = False
 
-N = int(input())
-for i in range(N):
+for _ in range(n):
     num = int(input())
-    # num이하 숫자까지 스택에 넣기
     while count <= num:
         stack.append(count)
         op.append('+')
         count += 1
-
-    # num이랑 스택 맨 위 숫자가 동일하다면 제거
+    
     if stack[-1] == num:
         stack.pop()
         op.append('-')
-    # 스택 수열을 만들 수 없으므로 NO
     else:
-        temp = False
+        fail = True
         break
-
-# 스택 수열을 만들수 있는지 여부에 따라 출력 
-if temp == False:
-    print("NO")
+    
+if fail:
+    print('NO')
 else:
-    for i in op:
-        print(i)
+    for o in op:
+        print(o)
