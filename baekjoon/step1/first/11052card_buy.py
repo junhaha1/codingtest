d231105c40be10ae087ab8da904dd2556e470b6b
junhaha1 <wnsgk919@naver.com>
2025-03-06 21:54:57 +0900

    꼭 다시 해보기

--- baekjoon/step1/first/11052card_buy.py
+++ baekjoon/step1/first/11052card_buy.py
@@ -2,4 +2,15 @@ from sys import stdin
 
 input= stdin.readline
 
-num = int(input())
+n = int(input())
+p = [0] + list(map(int, input().rstrip().split()))
+
+dp = [0] * (n+1)
+
+dp[1] = p[1]
+
+for i in range(2, n + 1):
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], dp[i-j] + p[j])
+
+print(dp[n])
