6898bb6fdff5fdc1397123a0f62243c498e7cdb0
junhaha1 <wnsgk919@naver.com>
2025-03-08 23:22:54 +0900

    success 다시 해보기

--- baekjoon/step1/first/10844step_count.py
+++ baekjoon/step1/first/10844step_count.py
@@ -4,12 +4,18 @@ input = stdin.readline
 
 n = int(input())
 
-dp = [0] * 101
-
-dp[1] = 9
+dp = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0] for _ in range(101)]
+dp[1] = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
 for i in range(2, n + 1):
-  dp[i] = (dp[i-1] * 2) -1
-  
-print(dp[n])
-  
+  for j in range(10):
+    if j == 0:
+      dp[i][0] = dp[i-1][1] % 1000000000
+    if j == 9:
+      dp[i][9] = dp[i-1][8] % 1000000000
+    if 1 <= j <=8:
+      dp[i][j] += dp[i-1][j - 1] % 1000000000
+      dp[i][j] += dp[i-1][j + 1] % 1000000000
+    
+
+print(sum(dp[n]) % 1000000000)
