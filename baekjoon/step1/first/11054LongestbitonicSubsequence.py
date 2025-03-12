d00e6c680d26d5fd810520b6f8c2822f0003f5a6
junhaha1 <wnsgk919@naver.com>
2025-03-12 10:43:14 +0900

    다시하기

---
+++ baekjoon/step1/first/11054LongestbitonicSubsequence.py
@@ -0,0 +1,45 @@
+from sys import stdin
+
+input = stdin.readline
+
+n = int(input())
+a = list(map(int, input().rstrip().split()))
+r_a = a[::-1]
+
+dp1 = [1] * n #증가하는
+dp2 = [1] * n #감소하는
+
+def bs1(a, lis):
+  s = 0
+  e = len(lis) - 1
+  while s <= e:
+    m = (s + e) // 2
+    if a > lis[m]:
+      s = m + 1
+    else:
+      e = m - 1
+  return s
+  
+i_lis = [a[0]]
+d_lis = [r_a[0]]
+
+for i in range(1, n):
+  if i_lis[-1] < a[i]:
+    i_lis.append(a[i])
+    dp1[i] = len(i_lis)
+  else:
+    idx = bs1(a[i], i_lis)
+    i_lis[idx] = a[i]
+    dp1[i] = len(i_lis[:idx + 1])
+  if d_lis[-1] < r_a[i]:
+    d_lis.append(r_a[i])
+    dp2[i] = len(d_lis)
+  else:
+    idx = bs1(r_a[i], d_lis)
+    d_lis[idx] = r_a[i]
+    dp2[i] = len(d_lis[:idx + 1])
+
+result = 0
+for i in range(n):
+  result = max(result, dp1[i] + dp2[-i-1] - 1)
+print(result)
