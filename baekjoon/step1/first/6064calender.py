4286ab02d0acd591a2c6848216212b0db6284888
junhaha1 <wnsgk919@naver.com>
2025-03-14 11:17:03 +0900

    꼭 다시하기

--- baekjoon/step1/first/6064calender.py
+++ baekjoon/step1/first/6064calender.py
@@ -1,17 +1,24 @@
 from sys import stdin
-
 input = stdin.readline
 
+def gcd(x, y):
+  while y:
+    x, y = y, x % y
+  return x
+
+#핵심
+#n과 m으로 나머지 연산을 했을 경우 x, y가 나오는 공통값을 찾는 문제
 for _ in range(int(input())):
   n, m, x, y = map(int, input().rstrip().split())
-  year = x
+  max_y = n * m // gcd(n, m)
+  
   while True:
-    if (year - x) % n ==0 and (year - y) % m == 0:
-      print(year)
-      break
-    
-    year += n
-    
-    if year > n*m:
+    if x > max_y:
       print(-1)
       break
+    if (x - y) % m == 0:
+      print(x)
+      break
+    x += n
+    
+    
