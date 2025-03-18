1e4a89e29f7611b7f51a6f879f9ae0fd054a1133
junhaha1 <wnsgk919@naver.com>
2025-03-18 10:36:12 +0900

    floid 연습용

---
+++ baekjoon/step1/first/11404floid.py
@@ -0,0 +1,31 @@
+from sys import stdin
+
+input = stdin.readline
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = min(graph[a][b], c)
+
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+            
+for gs in graph[1:]:
+  for g in gs[1:]:
+    if g == INF:
+      print(0, end = ' ')
+    else:
+      print(g, end=' ')
+  print()
