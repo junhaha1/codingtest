48b09031a2ed842bbf949d17f52182c1a4e3ac65
junhaha1 <wnsgk919@naver.com>
2025-03-24 23:02:59 +0900

    다시하기

--- baekjoon/step1/first/16928snakeandstepgame.py
+++ baekjoon/step1/first/16928snakeandstepgame.py
@@ -5,43 +5,39 @@ input = stdin.readline
 
 n, m  = map(int, input().rstrip().split())
 
-steps = [tuple(map(int, input().rstrip().split())) for _ in range(n)]
-steps = sorted(steps, key=lambda x : x[0])
-snakes = [tuple(map(int, input().rstrip().split())) for _ in range(m)]
-snakes = sorted(snakes, key=lambda x : x[0])
+ladder = {}
+snake = {}
 
-board = [int(1e9) for _ in range(101)]
+for _ in range(n):
+  a, b = map(int, input().rstrip().split())
+  ladder[a] = b
+
+for _ in range(m):
+  a, b = map(int, input().rstrip().split())
+  snake[a] = b
+
+board = [0] * 101
 
 def bfs(s):
   q = deque()
   q.append(s)
-  board[s] = 0
   
   while q:
-    pre_idx = q.popleft()
-    cur_idx = pre_idx
-    
-    for _ in range(6):
-      cur_idx += 1
-      if cur_idx <= 100: #이동한 좌표가 100보다 작거나 같으면
-        check = True
-        for st in steps:
-          if cur_idx == st[0] and board[pre_idx] + 1< board[st[1]]:
-            board[cur_idx] = board[pre_idx] + 1
-            board[st[1]] = board[cur_idx]
-            q.append(st[1])
-            check = False
-            break
-        for sn in snakes:
-          if cur_idx == sn[0] and board[pre_idx] + 1 < board[sn[1]]:
-            board[cur_idx] = board[pre_idx] + 1
-            board[sn[1]] = board[cur_idx]
-            q.append(sn[1])
-            check = False
-            break
-          
-        if check and board[cur_idx] > board[pre_idx] + 1:
-          board[cur_idx] = board[pre_idx] + 1
-          q.append(cur_idx)
+    x = q.popleft()
+    for i in range(1, 7):
+      next_x = x + i
+
+      if next_x <= 100 and board[next_x] == 0:
+
+        if next_x in ladder.keys():
+          next_x = ladder[next_x]
+
+        elif next_x in snake.keys():
+          next_x = snake[next_x]
+
+        if board[next_x] == 0:
+          board[next_x] = board[x] + 1
+          q.append(next_x)
+
 bfs(1)
 print(board[-1])
