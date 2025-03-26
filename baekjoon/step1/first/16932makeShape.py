2a32c0d748a384fa5c6a8222d0b6e8e59c833960
junhaha1 <wnsgk919@naver.com>
2025-03-26 21:52:51 +0900

    다시 해보기

--- baekjoon/step1/first/16932makeShape.py
+++ baekjoon/step1/first/16932makeShape.py
@@ -12,16 +12,18 @@ for _ in range(n):
     temp = list(map(int, input().rstrip().split()))
     board.append(temp)
 
+visited = [[False] * m for _ in range(n)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 max_shape = 0
 
-def bfs(cboard, x, y):
+def bfs(x, y, check):
     q = deque()
     cnt = 1
-    cboard[x][y] = -1
+    visited[x][y] = True
+    board[x][y] = check
     q.append((x, y))
 
     while q:
@@ -29,24 +31,35 @@ def bfs(cboard, x, y):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0<= ny < m and cboard[nx][ny] == 1:
+            if 0 <= nx < n and 0<= ny < m and board[nx][ny] == 1 and visited[nx][ny] == False:
                 cnt += 1
-                cboard[nx][ny] = -1
+                visited[nx][ny] = True
+                board[nx][ny] = check
                 q.append((nx, ny))
-    # print(cboard, cnt)
-    # input()
     return cnt
+
+#그룹화
+group = {}
+num = 1
+for x in range(n):
+    for y in range(m):
+        if board[x][y] == 1 and visited[x][y] == False:
+            value = bfs(x, y, num)
+            group[num] = value
+            num += 1
 
 for x in range(n):
     for y in range(m):
         if board[x][y] == 0:
+            temp = set()
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
-                if 0 <= nx < n and 0<= ny < m and board[nx][ny] == 1:
-                    #print(x, y)
-                    max_shape = max(max_shape, bfs(copy.deepcopy(board), x, y))
-                    break
+                if 0<= nx < n and 0 <= ny< m and board[nx][ny] in group.keys():
+                    temp.add(board[nx][ny])
+            temp_sum = 1
+            for t in temp:
+                temp_sum += group[t]
+            max_shape = max(max_shape, temp_sum)
 
 print(max_shape)
-
