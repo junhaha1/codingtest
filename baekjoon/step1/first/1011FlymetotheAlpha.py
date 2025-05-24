46f32f7198486810539794df5e19974973c29b3e
junhaha1 <wnsgk919@naver.com>
2025-05-24 22:51:29 +0900

    수학 + 시물레이션 - 다시 풀어보기

--- baekjoon/step1/first/1011FlymetotheAlpha.py
+++ baekjoon/step1/first/1011FlymetotheAlpha.py
@@ -1,4 +1,11 @@
-from sys import stdin
-from collections import deque
-
-input = stdin.readline
+for _ in range(int(input())):
+    cur, END = map(int, input().split())
+    distance = END - cur
+    n = int(distance ** 0.5)
+    
+    if n ** 2 == distance:
+        print(2*n-1)
+    if n ** 2 < distance <= n ** 2 + n:
+        print(2*n)
+    if n ** 2 + n < distance < (n + 1) ** 2:
+        print(2 * n + 1)
