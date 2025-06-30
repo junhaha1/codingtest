a84749584142b8fd70aea5d0638fc4b445ee4c74
junhaha1 <wnsgk919@naver.com>
2025-06-30 14:26:38 +0900

    DP - 문제 풀이 완료 (카카오 기출)

--- Programmers/스티커모으기(2).py
+++ Programmers/스티커모으기(2).py
@@ -1,7 +1,17 @@
 def solution(sticker):
-    answer = 0
+    dp = [[0] * len(sticker) for _ in range(2)]
+    if len(sticker) <= 1:
+        return sticker[0]
+    elif len(sticker) == 2:
+        return max(sticker[0], sticker[1])
+    else:   
+        dp[1][1] = sticker[1]
+        for i in range(len(sticker) - 1): #첫번째 선택했을 경우
+            dp[0][i] = max(dp[0][i-1], dp[0][i-2] + sticker[i])    
+        for i in range(1, len(sticker)): #두번째 선택했을 경우
+            dp[1][i] = max(dp[1][i-1], dp[1][i-2] + sticker[i])
 
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
-    return answer
+        return max(dp[0][len(sticker)-2], dp[1][len(sticker)-1])
+    
+    
+print(solution([14, 6, 5, 11, 3, 9, 2, 10]))
